0a2e647f2e9edeecdfccad202e55523651c78c0c
Climp228 <Climpser@gmail.com>
2019-11-30 19:09:57 +0200

    Merge remote-tracking branch 'origin2/master'
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.properties

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,5 +9,6 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new StartGame(), config);
 		// hello world
+		// kik kek cheburek
 	}
 }
